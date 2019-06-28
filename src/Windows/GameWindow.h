d780c8a0169f0fbf12b35d67d86b1e9f773bc1b3
Daniel Everland <danieleverland@gmail.com>
2019-06-28 20:59:46 +0200

    Included a bunch of header files in GameWindow

--- src/Windows/GameWindow.h
+++ src/Windows/GameWindow.h
@@ -1,4 +1,26 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+// Windows Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
+
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+#include "..\Direct X\d3dx12.h"
+
+// STL Headers
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+
 #include "Window.h"
+
+using namespace Microsoft::WRL;
 
 namespace ArtemisWindow
 {

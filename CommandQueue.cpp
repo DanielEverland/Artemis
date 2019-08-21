f3782f8ac4579c58d4066beea96d78cbf850e689
Daniel Everland <danieleverland@gmail.com>
2019-08-21 22:00:38 +0200

    Created CommandQueue class

---
+++ CommandQueue.cpp
@@ -0,0 +1,136 @@
+#include <cassert>
+
+#include "src/Direct X/DXHelper.h"
+#include "CommandQueue.h"
+
+CommandQueue::CommandQueue(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type)
+{
+	fenceValue = 0;
+	commandListType = type;
+	this->device = device;
+	
+	D3D12_COMMAND_QUEUE_DESC desc = {};
+	desc.Type = type;
+	desc.Priority = DefaultCommandQueuePriority;
+	desc.Flags = DefaultCommandQueueFlags;
+	desc.NodeMask = 0;
+
+	ThrowIfFailed(device->CreateCommandQueue(&desc, IID_PPV_ARGS(&commandQueue)));
+	ThrowIfFailed(device->CreateFence(fenceValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+
+	fenceEvent = CreateEvent(nullptr, false, false, nullptr);
+	assert(fenceEvent && "Failed to create fence event handle.");
+}
+
+CommandQueue::~CommandQueue()
+{
+}
+
+uint64_t CommandQueue::Signal()
+{
+	uint64_t fenceValue = ++this->fenceValue;
+	commandQueue->Signal(fence.Get(), this->fenceValue);
+	return fenceValue;
+}
+
+bool CommandQueue::IsFenceComplete(uint64_t fenceValue)
+{
+	return fence->GetCompletedValue() >= fenceValue;
+}
+
+void CommandQueue::WaitForFenceValue(uint64_t fenceValue)
+{
+	if (!IsFenceComplete(fenceValue))
+	{
+		fence->SetEventOnCompletion(fenceValue, fenceEvent);
+		WaitForSingleObject(fenceEvent, DWORD_MAX);
+	}
+}
+
+void CommandQueue::Flush()
+{
+	WaitForFenceValue(Signal());
+}
+
+ComPtr<ID3D12CommandAllocator> CommandQueue::CreateCommandAllocator()
+{
+	ComPtr<ID3D12CommandAllocator> commandAllocator;
+	
+	ThrowIfFailed(device->CreateCommandAllocator(commandListType, IID_PPV_ARGS(&commandAllocator)));
+
+	return commandAllocator;
+}
+
+ComPtr<ID3D12GraphicsCommandList2> CommandQueue::CreateCommandList(ComPtr<ID3D12CommandAllocator> allocator)
+{
+	ComPtr<ID3D12GraphicsCommandList2> commandList;
+
+	ThrowIfFailed(device->CreateCommandList(0, commandListType, allocator.Get(), nullptr, IID_PPV_ARGS(&commandList)));
+
+	return commandList;
+}
+
+ComPtr<ID3D12GraphicsCommandList2> CommandQueue::GetCommandList()
+{
+	ComPtr<ID3D12CommandAllocator> commandAllocator;
+	ComPtr<ID3D12GraphicsCommandList2> commandList;
+
+	if (!commandAllocatorQueue.empty() && IsFenceComplete(commandAllocatorQueue.front().fenceValue))
+	{
+		commandAllocator = commandAllocatorQueue.front().commandAllocator;
+		commandAllocatorQueue.pop();
+
+		ThrowIfFailed(commandAllocator->Reset());
+	}
+	else
+	{
+		commandAllocator = CreateCommandAllocator();
+	}
+
+	if (!commandListQueue.empty())
+	{
+		commandList = commandListQueue.front();
+		commandListQueue.pop();
+
+		ThrowIfFailed(commandList->Reset(commandAllocator.Get(), nullptr));
+	}
+	else
+	{
+		commandList = CreateCommandList(commandAllocator);
+	}
+
+	// Associate the command allocator with the command list so it can be retrieved when the command list is executed
+	ThrowIfFailed(commandList->SetPrivateDataInterface(__uuidof(ID3D12CommandAllocator), commandAllocator.Get()));
+
+	return commandList;
+}
+
+uint64_t CommandQueue::ExecuteCommandList(ComPtr<ID3D12GraphicsCommandList2> commandList)
+{
+	commandList->Close();
+
+	ID3D12CommandAllocator* commandAllocator;
+	UINT dataSize = sizeof(commandAllocator);
+
+	ThrowIfFailed(commandList->GetPrivateData(__uuidof(ID3D12CommandAllocator), &dataSize, &commandAllocator));
+
+	ID3D12CommandList* const commandLists[] =
+	{
+		commandList.Get(),
+	};
+
+	commandQueue->ExecuteCommandLists(1, commandLists);
+	uint64_t fenceValue = Signal();
+
+	commandAllocatorQueue.emplace(CommandAllocatorEntry{ fenceValue, commandAllocator });
+	commandListQueue.push(commandList);
+
+	commandAllocator->Release();
+
+	return fenceValue;
+}
+
+ComPtr<ID3D12CommandQueue> CommandQueue::GetD3D12CommandQueue() const
+{
+	return commandQueue;
+}
---
+++ CommandQueue.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <d3d12.h>
+#include <wrl.h>
+
+#include <cstdint>
+#include <queue>
+
+using namespace Microsoft::WRL;
+
+using std::queue;
+
+class CommandQueue
+{
+public:
+	explicit CommandQueue(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type);
+	virtual ~CommandQueue();
+
+	ComPtr<ID3D12GraphicsCommandList2> GetCommandList();
+	
+	uint64_t ExecuteCommandList(ComPtr<ID3D12GraphicsCommandList2> commandList);
+
+	uint64_t Signal();
+	bool IsFenceComplete(uint64_t fenceValue);
+	void WaitForFenceValue(uint64_t fenceValue);
+	void Flush();
+
+	ComPtr<ID3D12CommandQueue> GetD3D12CommandQueue() const;
+
+protected:
+	ComPtr<ID3D12CommandAllocator> CreateCommandAllocator();
+	ComPtr<ID3D12GraphicsCommandList2> CreateCommandList(ComPtr<ID3D12CommandAllocator> allocator);
+
+private:
+	
+	struct CommandAllocatorEntry
+	{
+		uint64_t fenceValue;
+		ComPtr<ID3D12CommandAllocator> commandAllocator;
+	};
+
+	static const D3D12_COMMAND_QUEUE_PRIORITY DefaultCommandQueuePriority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	static const D3D12_COMMAND_QUEUE_FLAGS DefaultCommandQueueFlags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+
+	D3D12_COMMAND_LIST_TYPE	commandListType;
+	ComPtr<ID3D12Device2> device;
+	ComPtr<ID3D12CommandQueue> commandQueue;
+
+	ComPtr<ID3D12Fence>	fence;
+	HANDLE fenceEvent;
+	uint64_t fenceValue;
+
+	queue<CommandAllocatorEntry> commandAllocatorQueue;
+	queue<ComPtr<ID3D12GraphicsCommandList2>> commandListQueue;
+};

185d1814335b932da7663c9a2e76decdd6a810c6
Daniel Everland <danieleverland@gmail.com>
2019-08-21 22:03:27 +0200

    Moved CommandQueue to proper folder

--- src/Direct X/CommandQueue.cpp
+++ src/Direct X/CommandQueue.cpp
@@ -1,6 +1,6 @@
 #include <cassert>
 
-#include "src/Direct X/DXHelper.h"
+#include "DXHelper.h"
 #include "CommandQueue.h"
 
 CommandQueue::CommandQueue(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type)

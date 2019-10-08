0877beb0cf938e21d1928d3c10ae11ba0017b4a9
Daniel Everland <danieleverland@gmail.com>
2019-10-08 18:18:16 +0200

    Fixed RTV initialization

--- src/Direct X/RenderTargetView.cpp
+++ src/Direct X/RenderTargetView.cpp
@@ -1,9 +1,10 @@
 #include "RenderTargetView.h"
+#include "DirectXHelper.h"
 
 RenderTargetView::RenderTargetView(const shared_ptr<const SwapChain> swapChain, const shared_ptr<const GraphicsDevice> graphicsDevice)
 {
-	swapChain->GetRawSwapChain().Get()->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(backBuffer.Get()));
-	graphicsDevice->GetRawDevice()->CreateRenderTargetView(backBuffer.Get(), 0, &renderTargetView);
+	ThrowIfFailed(swapChain->GetRawSwapChain()->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(backBuffer.GetAddressOf())));
+	ThrowIfFailed(graphicsDevice->GetRawDevice()->CreateRenderTargetView(backBuffer.Get(), 0, &renderTargetView));
 }
 
 ComPtr<ID3D11RenderTargetView> RenderTargetView::GetRawRenderTargetView() const

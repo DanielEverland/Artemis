7a9c98b02d926a009c04ceb80b3196b6383e64de
Daniel Everland <danieleverland@gmail.com>
2019-10-09 19:47:46 +0200

    Switched to a flip swapchain effect

--- src/Direct X/SwapChain.h
+++ src/Direct X/SwapChain.h
@@ -26,8 +26,8 @@ private:
 	const static DXGI_MODE_SCANLINE_ORDER ScanlineOrder = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
 	const static DXGI_MODE_SCALING Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 	const static DXGI_USAGE BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	const static DXGI_SWAP_EFFECT SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-	const static UINT BufferCount = 1;
+	const static DXGI_SWAP_EFFECT SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	const static UINT BufferCount = 2;
 	const static UINT Flags = 0;
 	
 	ComPtr<IDXGISwapChain> swapChain;

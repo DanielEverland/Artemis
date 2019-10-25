fff77123e117ccdf75e810ea65a7c9fd8966e1a3
Daniel Everland (Laptop) <danieleverland@gmail.com>
2019-10-25 13:47:37 +0200

    Added int vectors

---
+++ src/Engine/IntVector2.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Math/Vector2Base.h"
+
+using namespace ArtemisEngine::Math;
+
+struct IntVector2 : public Vector2Base<int>
+{
+};
---
+++ src/Engine/IntVector3.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Math/Vector3Base.h"
+
+using namespace ArtemisEngine::Math;
+
+struct IntVector3 : public Vector3Base<int>
+{
+};

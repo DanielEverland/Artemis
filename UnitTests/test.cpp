b60311c03ce379c604a7c0ff15da3cd8564860ed
Daniel Everland <danieleverland@gmail.com>
2019-12-10 20:41:22 +0100

    Fixed google test installations

--- UnitTests/test.cpp
+++ UnitTests/test.cpp
@@ -1,5 +1,20 @@
 #include "pch.h"
 
+#include "Engine/Vector2.h"
+
+TEST(Vector2, Addition)
+{
+    using ArtemisEngine::Vector2;
+
+    Vector2 a(12, -54);
+    Vector2 b(12.1, 54.34);
+
+    Vector2 c = a + b;
+
+    EXPECT_EQ(a[0] + b[0], c[0]);
+    EXPECT_EQ(a[1] + b[1], c[1]);
+}
+
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);

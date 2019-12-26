4a61904eb5a3caeea9a0eaed988c70bba43342fa
Daniel Everland <danieleverland@gmail.com>
2019-12-26 04:06:54 +0100

    Added some more vector2 unit tests

--- UnitTests/Math/Vector2Tests.cpp
+++ UnitTests/Math/Vector2Tests.cpp
@@ -11,7 +11,24 @@ namespace Math::Vectors
     {
         Vector2 vector(10.0, 20.25);
 
-        EXPECT_EQ(vector[0], 10);
-        EXPECT_EQ(vector[1], 20.25);
+        EXPECT_EQ(vector.x, 10.0);
+        EXPECT_EQ(vector.y, 20.25);
+    }
+    TEST(Vector2, IntConstructorXY)
+    {
+        Vector2 vector(1, 2);
+
+        EXPECT_EQ(vector.x, 1);
+        EXPECT_EQ(vector.y, 2);
+    }
+    TEST(Vector2, MemberAssignment)
+    {
+        Vector2 vector;
+
+        vector.x = 5;
+        vector.y = 10;
+
+        EXPECT_EQ(vector.x, 5);
+        EXPECT_EQ(vector.y, 10);
     }
 }

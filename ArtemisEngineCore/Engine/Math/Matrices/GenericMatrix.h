c1be228b210f05fab2d7386e07a47aea5348c484
Daniel Everland <danieleverland@gmail.com>
2020-01-11 21:33:15 +0100

    Progress on matrixes

--- ArtemisEngineCore/Engine/Math/Matrices/GenericMatrix.h
+++ ArtemisEngineCore/Engine/Math/Matrices/GenericMatrix.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <type_traits>
+#include <initializer_list>
+#include <algorithm>
+
+#include "Exceptions/InvalidArgumentException.h"
+#include "Engine/Math/Vectors/VectorBase.h"
 #include "Debugging/IDebugStringReturner.h"
+
+using ArtemisEngine::Math::Vectors::VectorBase;
+using std::initializer_list;
 
 namespace ArtemisEngine::Math::Matrices
 {
@@ -8,24 +17,227 @@ namespace ArtemisEngine::Math::Matrices
 	class GenericMatrix
 	{
 	public:
-		// Returns the dot product of two matrices
-		template<unsigned int otherRows, unsigned int otherColumns>
-		static T DotProduct(GenericMatrix& a, GenericMatrix<T, otherRows, otherColumns>& b, unsigned int aRow, unsigned int bColumn);
+		class ValueProxy;
+
+		~GenericMatrix() = default;
+		GenericMatrix() = default;
+		
+		GenericMatrix(const GenericMatrix& copy)
+		{
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				for (unsigned int j = 0; j < columns; j++)
+				{
+					values[i][j] = copy.values[i][j];
+				}
+			}
+		}
+		GenericMatrix(GenericMatrix&& move)
+		{
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				for (unsigned int j = 0; j < columns; j++)
+				{
+					values[i][j] = move.values[i][j];
+
+					move.values[i][j] = 0;
+				}
+			}
+		}
+
+		
+
+		template<std::size_t N>
+		GenericMatrix(const VectorBase<T, columns>(&rowVectors)[N])
+		{
+			static_assert(N <= rows, "Passed more row vectors to matrix constructor than there are rows");
+
+			for (unsigned int i = 0; i < N; i++)
+			{
+				SetRow(i, rowVectors[i]);
+			}
+		}
+
+		T* operator[](int rowIndex)
+		{
+			return values[rowIndex];
+		}
+		const T* operator[](int rowIndex) const
+		{
+			return values[rowIndex];
+		}
+
+		constexpr unsigned int GetRows() const
+		{
+			return rows;
+		}
+
+		constexpr unsigned int GetColumns() const
+		{
+			return columns;
+		}
+
+		T GetValue(unsigned int row, unsigned int column)
+		{
+			return values[row][column];
+		}
+
+		void SetRow(int rowIndex, VectorBase<T, columns> rowValues)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				values[rowIndex][j] = rowValues[j];
+			}
+		}
+		void SetColumn(int columnIndex, VectorBase<T, rows> columnValues)
+		{
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				values[i][columnIndex] = columnValues[i];
+			}
+		}
 
 		// Returns the transpose of this matrix.
 		// The transpose turns the columns and rows.
-		GenericMatrix GetTranspose() const;
+		GenericMatrix GetTranspose() const
+		{
+			GenericMatrix<T, columns, rows> transpose;
 
-		template<unsigned int otherRows, unsigned int otherColumns>
-		GenericMatrix<T, columns, otherRows>& operator*(GenericMatrix<T, otherRows, otherColumns>& other);
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				for (unsigned int j = 0; j < columns; j++)
+				{
+					transpose->values[j, i] = this->values[i, j];
+				}
+			}
 
-		GenericMatrix& operator+(GenericMatrix& other);
-		GenericMatrix& operator-(GenericMatrix& other);
+			return transpose;
+		}
 
-		template<class TScalar>
-		GenericMatrix& operator*(TScalar scalar);
-		
+		GenericMatrix& operator=(GenericMatrix& copy)
+		{
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				for (unsigned int j = 0; j < columns; j++)
+				{
+					values[i][j] = copy.values[i][j];
+				}
+			}
+
+			return *this;
+		}
+		GenericMatrix& operator=(GenericMatrix&& move)
+		{
+			for (unsigned int i = 0; i < rows; i++)
+			{
+				for (unsigned int j = 0; j < columns; j++)
+				{
+					values[i][j] = move.values[i][j];
+
+					move.values[i][j] = 0;
+				}
+			}
+
+			return *this;
+		}
+
 	private:
 		T values[rows][columns] = { };
 	};
+
+	template<class T, unsigned int rows, unsigned int columns>
+	GenericMatrix<T, rows, columns> operator+(const GenericMatrix<T, rows, columns>& a, const GenericMatrix<T, rows, columns>& b)
+	{
+		GenericMatrix<T, rows, columns> newMatrix;
+
+		for (unsigned int i = 0; i < rows; i++)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				newMatrix[i][j] = a[i][j] + b[i][j];
+			}
+		}
+
+		return newMatrix;
+	}
+
+	template<class T, unsigned int rows, unsigned int columns>
+	GenericMatrix<T, rows, columns> operator+=(GenericMatrix<T, rows, columns>& a, const GenericMatrix<T, rows, columns>& b)
+	{
+		for (unsigned int i = 0; i < rows; i++)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				a[i][j] += b[i][j];
+			}
+		}
+
+		return a;
+	}
+
+	template<class T, unsigned int rows, unsigned int columns>
+	GenericMatrix<T, rows, columns> operator-(const GenericMatrix<T, rows, columns>& a, const GenericMatrix<T, rows, columns>& b)
+	{
+		GenericMatrix<T, rows, columns> newMatrix;
+
+		for (unsigned int i = 0; i < rows; i++)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				newMatrix[i][j] = a[i][j] - b[i][j];
+			}
+		}
+		
+		return newMatrix;
+	}
+
+	template<class T, unsigned int rows, unsigned int columns>
+	GenericMatrix<T, rows, columns> operator-=(GenericMatrix<T, rows, columns>& a, const GenericMatrix<T, rows, columns>& b)
+	{
+		for (unsigned int i = 0; i < rows; i++)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				a[i][j] -= b[i][j];
+			}
+		}
+
+		return a;
+	}
+
+	template<class T, unsigned int aRows, unsigned int aColumns, unsigned int bRows, unsigned int bColumns>
+	GenericMatrix<T, bRows, aColumns> operator*(const GenericMatrix<T, aRows, aColumns>& a, const GenericMatrix<T, bRows, bColumns>& b)
+	{
+		if (bRows != aColumns)
+			throw InvalidArgumentException("Cannot define matrix multiplication when a's number of columns isn't equal to b's number of rows.");
+
+		unsigned int newRowCount = bRows;
+		unsigned int newColumnCount = aColumns;
+
+		GenericMatrix<T, newRowCount, newColumnCount> newMatrix;
+
+		for (unsigned int i = 0; i < newRowCount; i++)
+		{
+			for (unsigned int j = 0; j < newColumnCount; j++)
+			{
+				newMatrix[i, j] = GenericMatrix::DotProduct(a, b, i, j);
+			}
+		}
+
+		return newMatrix;
+	}
+
+	template<class T, unsigned int rows, unsigned int columns, class TScalar>
+	GenericMatrix<T, rows, columns> operator*(GenericMatrix<T, rows, columns> matrix, TScalar scalar)
+	{
+		for (unsigned int i = 0; i < rows; i++)
+		{
+			for (unsigned int j = 0; j < columns; j++)
+			{
+				matrix[i][j] *= scalar;
+			}
+		}
+
+		return matrix;
+	}
 }
---
+++ UnitTests/Math/MatrixTests.cpp
@@ -0,0 +1,150 @@
+#include "pch.h"
+
+#include "Engine/Math/Matrices/GenericMatrix.h"
+#include "Engine/Math/Vectors/VectorBase.h"
+
+namespace Math::Matrices
+{
+	using ArtemisEngine::Math::Matrices::GenericMatrix;
+	using ArtemisEngine::Math::Vectors::VectorBase;
+
+	typedef double T;
+	const unsigned int rows = 4;
+	const unsigned int columns = 4;
+
+	typedef GenericMatrix<T, rows, columns> Matrix;
+	typedef VectorBase<T, columns> RowVector;
+
+	const static RowVector TestValues[2][rows]
+	{
+		{
+			RowVector(1.0, 2.50, -1.25, 0),
+			RowVector(-2.75, 1.25, 0.25, 1.25),
+			RowVector(3.25, -1.0, 2.75, 4.25),
+			RowVector(-3.0, -1.25, 2.0, 1.0)
+		},
+		{
+			RowVector(4.25, -1.25, 3.0, 5.25),
+			RowVector(-1.0, 1.25, 2.0, 3.25),
+			RowVector(-0.75, -1.25, -2.0, 3.25),
+			RowVector(2.50, -1.50, -1.75, 2.0),
+		}
+	};
+
+	Matrix GetTestMatrix(const RowVector* rowVectorArray)
+	{
+		return Matrix({ rowVectorArray[0], rowVectorArray[1], rowVectorArray[2], rowVectorArray[3] });
+	}
+
+	TEST(MatrixTest, EmptyConstructor)
+	{
+		Matrix matrix;
+
+		for (unsigned int i = 0; i < matrix.GetRows(); i++)
+		{
+			for (unsigned j = 0; j < matrix.GetColumns(); j++)
+			{
+				EXPECT_EQ(0, matrix.GetValue(i, j));
+			}
+		}
+	}
+	TEST(MatrixTest, InitializerListConstructor)
+	{
+		const RowVector* UsedTestValues = TestValues[0];
+
+		Matrix matrix({ UsedTestValues[0], UsedTestValues[1], UsedTestValues[2], UsedTestValues[3] });
+
+		for (unsigned int i = 0; i < matrix.GetRows(); i++)
+		{
+			for (unsigned int j = 0; j < matrix.GetColumns(); j++)
+			{
+				EXPECT_EQ(UsedTestValues[i][j], matrix.GetValue(i, j));
+			}
+		}
+	}
+	TEST(MatrixTest, MultiDimensionalIndexer)
+	{
+		const RowVector* testValuesRowVector = TestValues[0];
+		Matrix matrix = GetTestMatrix(testValuesRowVector);
+
+		for (unsigned int i = 0; i < matrix.GetRows(); i++)
+		{
+			for (unsigned int j = 0; j < matrix.GetColumns(); j++)
+			{
+				EXPECT_EQ(testValuesRowVector[i][j], matrix[i][j]);
+			}
+		}
+	}
+	TEST(MatrixTest, Addition)
+	{
+		const RowVector* aTestValues = TestValues[0];
+		const RowVector* bTestValues = TestValues[1];
+
+		Matrix a = GetTestMatrix(aTestValues);
+		Matrix b = GetTestMatrix(bTestValues);
+
+		Matrix c = a + b;
+
+		for (unsigned int i = 0; i < c.GetRows(); i++)
+		{
+			for (unsigned int j = 0; j < c.GetColumns(); j++)
+			{
+				EXPECT_EQ(aTestValues[i][j] + bTestValues[i][j], c[i][j]);
+			}
+		}
+	}
+	TEST(MatrixTest, AdditionAssignment)
+	{
+		const RowVector* aTestValues = TestValues[0];
+		const RowVector* bTestValues = TestValues[1];
+
+		Matrix a = GetTestMatrix(aTestValues);
+		Matrix b = GetTestMatrix(bTestValues);
+
+		a += b;
+
+		for (unsigned int i = 0; i < a.GetRows(); i++)
+		{
+			for (unsigned int j = 0; j < a.GetColumns(); j++)
+			{
+				EXPECT_EQ(aTestValues[i][j] + bTestValues[i][j], a[i][j]);
+			}
+		}
+	}
+	TEST(MatrixTest, Subtraction)
+	{
+		const RowVector* aTestValues = TestValues[0];
+		const RowVector* bTestValues = TestValues[1];
+
+		Matrix a = GetTestMatrix(aTestValues);
+		Matrix b = GetTestMatrix(bTestValues);
+
+		Matrix c = a - b;
+
+		for (unsigned int i = 0; i < c.GetRows(); i++)
+		{
+			for (unsigned int j = 0; j < c.GetColumns(); j++)
+			{
+				EXPECT_EQ(aTestValues[i][j] - bTestValues[i][j], c[i][j]);
+			}
+		}
+	}
+	TEST(MatrixTest, SubtractionAssignment)
+	{
+		const RowVector* aTestValues = TestValues[0];
+		const RowVector* bTestValues = TestValues[1];
+
+		Matrix a = GetTestMatrix(aTestValues);
+		Matrix b = GetTestMatrix(bTestValues);
+
+		a -= b;
+
+		for (unsigned int i = 0; i < a.GetRows(); i++)
+		{
+			for (unsigned int j = 0; j < a.GetColumns(); j++)
+			{
+				EXPECT_EQ(aTestValues[i][j] - bTestValues[i][j], a[i][j]);
+			}
+		}
+	}
+}

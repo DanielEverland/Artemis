80546cfeb43cee006cdc6185b8071c6b7293b5b1
Daniel Everland <danieleverland@gmail.com>
2020-02-01 20:13:26 +0100

    Refactored math functions

--- ArtemisEngineCore/Engine/Math.cpp
+++ ArtemisEngineCore/Engine/Math.cpp
@@ -1,4 +1,6 @@
 #include "Math.h"
 
-
-
+const double Math::Pi = M_PI;
+const double Math::RadToDeg = 180.0 / Math::Pi;
+const double Math::DegToRad = Math::Pi / 180.0;
+const double Math::Infinity = std::numeric_limits<double>::infinity();
--- ArtemisEngineCore/Engine/Math.h
+++ ArtemisEngineCore/Engine/Math.h
@@ -16,43 +16,26 @@ public:
 	static const double Infinity;
 
 	template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr >
-	static bool IsFloatingPointIntegral(const T& value);
+	static bool IsFloatingPointIntegral(const T& value)
+	{
+		return std::trunc(value) == value;
+	}
 
 	template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr >
-	static bool IsPositiveInfinity(const T& value);
+	static bool IsPositiveInfinity(const T& value)
+	{
+		return value == Infinity;
+	}
 
 	template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr >
-	static bool IsNegativeInfinity(const T& value);
+	static bool IsNegativeInfinity(const T& value)
+	{
+		return value == -Infinity;
+	}
 
 	template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr >
-	static bool IsNaN(const T& value);
+	static bool IsNaN(const T& value)
+	{
+		return isnan(value);
+	}
 };
-
-inline const double Math::Pi = M_PI;
-inline const double Math::RadToDeg = 180.0 / Math::Pi;
-inline const double Math::DegToRad = Math::Pi / 180.0;
-inline const double Math::Infinity = std::numeric_limits<double>::infinity();
-
-template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type *>
-bool Math::IsFloatingPointIntegral(const T& value)
-{
-	return std::trunc(value) == value;
-}
-
-template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type*>
-bool Math::IsPositiveInfinity(const T& value)
-{
-	return value == Infinity;
-}
-
-template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type*>
-bool Math::IsNegativeInfinity(const T& value)
-{
-	return value == -Infinity;
-}
-
-template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type*>
-inline bool Math::IsNaN(const T& value)
-{
-	return isnan(value);
-}

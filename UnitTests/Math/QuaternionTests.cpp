2db4c85f99783ecbb2c14b60b44105a2e98a5d1b
Daniel Everland <danieleverland@gmail.com>
2020-02-06 20:03:06 +0100

    Implemented test for quaternion addition

--- UnitTests/Math/QuaternionTests.cpp
+++ UnitTests/Math/QuaternionTests.cpp
@@ -50,3 +50,16 @@ TEST(Quaternions, Multiplication)
 
 	ExpectEqual(expectedResult, actualResult);
 }
+
+TEST(Quaternions, Addition)
+{
+	Quaternion expectedResult(0.0902, 0.4685, 0.08884000000000003, 0.000030000000000002247);
+	Quaternion quaternionA(0.4353, 0.1234, -0.3465, 0.23523);
+	Quaternion quaternionB(-0.3451, 0.3451, 0.43534, -0.2352);
+
+
+	Quaternion actualResult = quaternionA + quaternionB;
+
+
+	ExpectEqual(expectedResult, actualResult);
+}

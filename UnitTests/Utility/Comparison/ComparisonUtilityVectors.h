3fcb7e9aa4b2682f75fb24360bdc37d52b002e4a
Daniel Everland <danieleverland@gmail.com>
2020-02-12 12:51:46 +0100

    Cleaned up vector test utility functions

--- UnitTests/Utility/Comparison/ComparisonUtilityVectors.h
+++ UnitTests/Utility/Comparison/ComparisonUtilityVectors.h
@@ -1,21 +1,7 @@
 #pragma once
 
 #include "ComparisonUtility.h"
-#include "Engine/Maths/Vectors/VectorBase.h"
 
 #define VectorExpectNear(expectedResult, actualResult) \
 	for (unsigned int i = 0; i < expectedResult.GetDimensions(); i++) \
 		TempExpectNear(expectedResult[i], actualResult[i]);
-
-using ArtemisEngine::Maths::Vectors::VectorBase;
-
-namespace ArtemisEngine::UnitTests::Comparisons
-{
-	template<class T, unsigned int dimensions>
-	inline void ExpectNear(const VectorBase<T, dimensions>& a, const VectorBase<T, dimensions>& b)
-	{
-		EXPECT_NEAR(a.X, b.X, NearComparisonPrecision);
-		EXPECT_NEAR(a.Y, b.Y, NearComparisonPrecision);
-		EXPECT_NEAR(a.Z, b.Z, NearComparisonPrecision);
-	}
-}

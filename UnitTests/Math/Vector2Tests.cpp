b9a429836e62cba48c904b4bfb72c43d257a5fcf
Daniel Everland <danieleverland@gmail.com>
2020-02-14 22:39:14 +0100

    Added spaces to Vector2 tests

--- UnitTests/Math/Vector2Tests.cpp
+++ UnitTests/Math/Vector2Tests.cpp
@@ -11,12 +11,14 @@ namespace Maths::Vectors
     {
         Vector2 vector(10.0, 20.25);
 
+
         TempExpectNear(10.0, vector.X);
         TempExpectNear(20.25, vector.Y);
     }
     TEST(Vector2, InitializerListConstructor)
     {
         Vector2 vector({ 43.51, 2.0, 12.3 });
+
 
         TempExpectNear(43.51, vector.X);
         TempExpectNear(2.0, vector.Y);
@@ -25,6 +27,7 @@ namespace Maths::Vectors
     {
         Vector2 vector(1, 2);
 
+
         TempExpectEqual(1, vector.X);
         TempExpectEqual(2, vector.Y);
     }
@@ -32,8 +35,10 @@ namespace Maths::Vectors
     {
         Vector2 vector;
 
+
         vector.X = 5;
         vector.Y = 10;
+
 
         TempExpectEqual(5, vector.X);
         TempExpectEqual(10, vector.Y);

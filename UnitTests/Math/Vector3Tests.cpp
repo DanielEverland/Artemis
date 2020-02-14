8f4609b8682003abc2863a6787ce81ca618bea25
Daniel Everland <danieleverland@gmail.com>
2020-02-14 22:38:40 +0100

    Refactored all Vector3 tests

--- UnitTests/Math/Vector3Tests.cpp
+++ UnitTests/Math/Vector3Tests.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "pch.h"
 
+#include "Utility/Comparison/ComparisonUtility.h"
 #include "Utility/Comparison/ComparisonUtilityVectors.h"
 #include "Engine/Vector3.h"
 #include "Engine/Vector2.h"
@@ -13,62 +14,71 @@ namespace Maths::Vectors
     {
         Vector3 vector(10.0, 20.25);
 
-        EXPECT_EQ(10.0, vector.X);
-        EXPECT_EQ(20.25, vector.Y);
+
+        TempExpectNear(10.0, vector.X);
+        TempExpectNear(20.25, vector.Y);
     }
     TEST(Vector3, ConstructorXYZ)
     {
         Vector3 vector(10.0, 20.25, 30.20);
 
-        EXPECT_EQ(10.0, vector.X);
-        EXPECT_EQ(20.25, vector.Y);
-        EXPECT_EQ(30.20, vector.Z);
+
+        TempExpectNear(10.0, vector.X);
+        TempExpectNear(20.25, vector.Y);
+        TempExpectNear(30.20, vector.Z);
     }
     TEST(Vector3, InitializerListConstructor)
     {
         Vector3 vector({ 43.51, 2.0, 12.3, 153.0154 });
 
-        EXPECT_EQ(43.51, vector.X);
-        EXPECT_EQ(2.0, vector.Y);
-        EXPECT_EQ(12.3, vector.Z);
+
+        TempExpectNear(43.51, vector.X);
+        TempExpectNear(2.0, vector.Y);
+        TempExpectNear(12.3, vector.Z);
     }
     TEST(Vector3, IntConstructorXY)
     {
         Vector3 vector(1, 2);
 
-        EXPECT_EQ(1, vector.X);
-        EXPECT_EQ(2, vector.Y);
+
+        TempExpectEqual(1, vector.X);
+        TempExpectEqual(2, vector.Y);
     }
     TEST(Vector3, IntConstructorXYZ)
     {
         Vector3 vector(1, 2, 3);
 
-        EXPECT_EQ(1, vector.X);
-        EXPECT_EQ(2, vector.Y);
-        EXPECT_EQ(3, vector.Z);
+
+        TempExpectEqual(1, vector.X);
+        TempExpectEqual(2, vector.Y);
+        TempExpectEqual(3, vector.Z);
     }
     TEST(Vector3, MemberAssignment)
     {
         Vector3 vector;
 
+
         vector.X = 5;
         vector.Y = 10;
         vector.Z = 1;
 
-        EXPECT_EQ(5, vector.X);
-        EXPECT_EQ(10, vector.Y);
-        EXPECT_EQ(1, vector.Z);
+
+        TempExpectEqual(5, vector.X);
+        TempExpectEqual(10, vector.Y);
+        TempExpectEqual(1, vector.Z);
     }
     TEST(Vector3, CrossProduct)
     {
         Vector3 a(1, 2, 3);
         Vector3 b(4, 5, 6);
 
+
         Vector3 c = Vector3::GetCrossProduct(a, b);
 
-        EXPECT_EQ(-3, c.X);
-        EXPECT_EQ(6, c.Y);
-        EXPECT_EQ(-3, c.Z);
+
+        TempExpectEqual(-3, c.X);
+        TempExpectEqual(6, c.Y);
+        TempExpectEqual(-3, c.Z);
     }
     TEST(Vector3, UpgradeVector2)
     {

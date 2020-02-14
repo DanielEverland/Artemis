71043eb9d09d2e3b04f8779d350a469e9252ddea
Daniel Everland <danieleverland@gmail.com>
2020-02-14 22:40:37 +0100

    Refactored all Vector4 tests

--- UnitTests/Math/Vector4Tests.cpp
+++ UnitTests/Math/Vector4Tests.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "pch.h"
 
+#include "Utility/Comparison/ComparisonUtility.h"
 #include "Utility/Comparison/ComparisonUtilityVectors.h"
 #include "Engine/Vector4.h"
 #include "Engine/Vector2.h"
@@ -16,72 +17,81 @@ namespace Maths::Vectors
     {
         Vector4 vector({ 43.51, 2.0, 12.3, 153.0154, 12453.123 });
 
-        EXPECT_EQ(43.51, vector.X);
-        EXPECT_EQ(2.0, vector.Y);
-        EXPECT_EQ(12.3, vector.Z);
-        EXPECT_EQ(153.0154, vector.W);
+
+        TempExpectNear(43.51, vector.X);
+        TempExpectNear(2.0, vector.Y);
+        TempExpectNear(12.3, vector.Z);
+        TempExpectNear(153.0154, vector.W);
     }
     TEST(Vector4, ConstructorXY)
     {
         Vector4 vector(10.0, 20.25);
 
-        EXPECT_EQ(10.0, vector.X);
-        EXPECT_EQ(20.25, vector.Y);
+
+        TempExpectNear(10.0, vector.X);
+        TempExpectNear(20.25, vector.Y);
     }
     TEST(Vector4, ConstructorXYZ)
     {
         Vector4 vector(10.0, 20.25, 30.20);
 
-        EXPECT_EQ(10.0, vector.X);
-        EXPECT_EQ(20.25, vector.Y);
-        EXPECT_EQ(30.20, vector.Z);
+
+        TempExpectNear(10.0, vector.X);
+        TempExpectNear(20.25, vector.Y);
+        TempExpectNear(30.20, vector.Z);
     }
     TEST(Vector4, ConstructorXYZW)
     {
         Vector4 vector(10.0, 20.25, 30.20, 40.12);
 
-        EXPECT_EQ(10.0, vector.X);
-        EXPECT_EQ(20.25, vector.Y);
-        EXPECT_EQ(30.20, vector.Z);
-        EXPECT_EQ(40.12, vector.W);
+
+        TempExpectNear(10.0, vector.X);
+        TempExpectNear(20.25, vector.Y);
+        TempExpectNear(30.20, vector.Z);
+        TempExpectNear(40.12, vector.W);
     }
     TEST(Vector4, IntConstructorXY)
     {
         Vector4 vector(1, 2);
 
-        EXPECT_EQ(1, vector.X);
-        EXPECT_EQ(2, vector.Y);
+
+        TempExpectEqual(1, vector.X);
+        TempExpectEqual(2, vector.Y);
     }
     TEST(Vector4, IntConstructorXYZ)
     {
         Vector4 vector(1, 2, 3);
 
-        EXPECT_EQ(1, vector.X);
-        EXPECT_EQ(2, vector.Y);
-        EXPECT_EQ(3, vector.Z);
+
+        TempExpectEqual(1, vector.X);
+        TempExpectEqual(2, vector.Y);
+        TempExpectEqual(3, vector.Z);
     }
     TEST(Vector4, IntConstructorXYZW)
     {
         Vector4 vector(1, 2, 3, 4);
 
-        EXPECT_EQ(1, vector.X);
-        EXPECT_EQ(2, vector.Y);
-        EXPECT_EQ(3, vector.Z);
-        EXPECT_EQ(4, vector.W);
+
+        TempExpectEqual(1, vector.X);
+        TempExpectEqual(2, vector.Y);
+        TempExpectEqual(3, vector.Z);
+        TempExpectEqual(4, vector.W);
     }
     TEST(Vector4, MemberAssignment)
     {
         Vector4 vector;
+
 
         vector.X = 5;
         vector.Y = 10;
         vector.Z = 1;
         vector.W = 100;
 
-        EXPECT_EQ(5, vector.X);
-        EXPECT_EQ(10, vector.Y);
-        EXPECT_EQ(1, vector.Z);
-        EXPECT_EQ(100, vector.W);
+
+        TempExpectEqual(5, vector.X);
+        TempExpectEqual(10, vector.Y);
+        TempExpectEqual(1, vector.Z);
+        TempExpectEqual(100, vector.W);
     }
     TEST(Vector4, UpgradeVector2)
     {

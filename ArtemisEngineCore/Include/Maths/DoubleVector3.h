8cef6f55a57e2b642bbca253244fb97ecec25a2d
Daniel Everland <danieleverland@gmail.com>
2020-05-17 15:49:56 +0200

    Added vector constants

--- ArtemisEngineCore/Include/Maths/DoubleVector3.h
+++ ArtemisEngineCore/Include/Maths/DoubleVector3.h
@@ -7,5 +7,13 @@ namespace ArtemisEngine::Maths::Vectors
 	struct DoubleVector3 : public Vector3Base<double>
 	{
 		using Vector3Base::Vector3Base;
+
+		static const DoubleVector3 Zero;
+		static const DoubleVector3 Up;
+		static const DoubleVector3 Down;
+		static const DoubleVector3 Right;
+		static const DoubleVector3 Left;
+		static const DoubleVector3 Forward;
+		static const DoubleVector3 Back;
 	};
 }
---
+++ ArtemisEngineCore/Source/Maths/DoubleVector3.cpp
@@ -0,0 +1,11 @@
+#include "Include/Maths/DoubleVector3.h"
+
+using namespace Maths::Vectors;
+
+const DoubleVector3 Zero = DoubleVector3(0.0, 0.0, 0.0);
+const DoubleVector3 Up = DoubleVector3(0.0, 1.0, 0.0);
+const DoubleVector3 Down = DoubleVector3(0.0, -1.0, 0.0);
+const DoubleVector3 Right = DoubleVector3(1.0, 0.0, 0.0);
+const DoubleVector3 Left = DoubleVector3(-1.0, 0.0, 0.0);
+const DoubleVector3 Forward = DoubleVector3(0.0, 0.0, 1.0);
+const DoubleVector3 Back = DoubleVector3(0.0, 0.0, -1.0);

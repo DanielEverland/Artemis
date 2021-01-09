7e5be33007e4007ca81dfa15747c48da67a2e001
Daniel Everland <danieleverland@gmail.com>
2021-01-09 20:21:26 +0100

    Purged 3D from main

--- UnitTests/Utility/Comparison/ComparisonUtilityQuaternions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ComparisonUtility.h"
-
-#define QuaternionExpectNear(expectedResult, actualResult) \
-	ExpectNear(expectedResult.X, actualResult.X); \
-	ExpectNear(expectedResult.Y, actualResult.Y); \
-	ExpectNear(expectedResult.Z, actualResult.Z); \
-	ExpectNear(expectedResult.W, actualResult.W);
--- UnitTests/Utility/Comparison/ComparisonUtilityVectors.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ComparisonUtility.h"
-
-#define VectorExpectNear(expectedResult, actualResult) \
-	for (unsigned int i = 0; i < expectedResult.GetDimensions(); i++) \
-		ExpectNear(expectedResult[i], actualResult[i]);

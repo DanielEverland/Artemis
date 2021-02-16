55f2cc7c2c687f8506eeb1840c81d89c3a9675ba
Daniel Everland <danieleverland@gmail.com>
2021-02-16 20:48:22 +0100

    Fixed macro redefinition

--- Core/IO/Json.h
+++ Core/IO/Json.h
@@ -10,6 +10,7 @@ using std::string;
 using namespace nlohmann;
 
 // Redefine to allow for missing fields in JSON strings
+#undef NLOHMANN_JSON_FROM
 #define NLOHMANN_JSON_FROM(v1) if(nlohmann_json_j.contains(#v1)) { nlohmann_json_j.at(#v1).get_to(nlohmann_json_t.v1); }
 
 #define JSON_DEFINE(Type, ...) NLOHMANN_DEFINE_TYPE_INTRUSIVE(Type, __VA_ARGS__)

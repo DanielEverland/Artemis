37ebf865d940febce8804f9e4c8ced4403e4b091
Daniel Everland <danieleverland@gmail.com>
2021-05-21 21:44:39 +0200

    Made warning permanent in utility

--- Engine/Source/Memory/AllocatorUtility.cpp
+++ Engine/Source/Memory/AllocatorUtility.cpp
@@ -2,10 +2,11 @@
 
 using namespace ArtemisEngine;
 
-#pragma warning(disable:4302)
 uint8 AllocatorUtility::GetMemoryAddressAdjustment(void* address, uint8 alignment)
 {
+#pragma warning(disable:4302)
 	const uint8 adjustment = alignment - (reinterpret_cast<uint8>(address) & static_cast<uint8>(alignment - 1));
+#pragma warning(default:4302)
 
 	if (adjustment == alignment)
 		return 0;
